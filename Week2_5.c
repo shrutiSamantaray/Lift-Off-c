0ef961138fdc7b63730603999c3d565a0fe4b4d3
shrutisamantaray333 <98402399+shrutisamantaray333@users.noreply.github.com>
2022-05-02 17:54:10 +0530

    Update Week2_5.c

--- Week2_5.c
+++ Week2_5.c
@@ -1,58 +1,41 @@
 #include <stdio.h>
+
 int main()
 {
-    int arr[2][2];
-    int sum = 0;
-    int arr2[2][2];
-    int prod = 1;
-
-    printf("Enter the number for the first 2*2 array:\n");
-
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++){
+    int arr[2][2], arr1[2][2], i, j, arr2[2][2], arr3[2][2];
+    printf("Input the values of 2X2 matrice : \n");
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 2; j++)
+        {
+            printf("a%d%d : ", i + 1, j + 1);
             scanf("%d", &arr[i][j]);
         }
-    }
-    for (int i = 0; i < 2; i++)
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 2; j++)
+        {
+            printf("b%d%d : ", i + 1, j + 1);
+            scanf("%d", &arr1[i][j]);
+        }
+    printf("The sum of matrices\n");
+    for (i = 0; i < 2; i++)
     {
-        for (int j = 0; j < 2; j++){
-            printf("%d ", arr[i][j]);
+        for (j = 0; j < 2; j++)
+        {
+            arr2[i][j] = arr[i][j] + arr1[i][j];
+            printf("%d  ", arr2[i][j]);
         }
         printf("\n");
     }
-    printf("Enter the numbers for the second array :\n");
-    for (int i = 0; i < 2; i++)
+    arr3[0][0] = arr[0][0] * arr1[0][0] + arr[0][1] * arr1[1][0];
+    arr3[0][1] = arr[0][0] * arr1[0][1] + arr[0][1] * arr1[1][1];
+    arr3[1][0] = arr[1][0] * arr1[0][0] + arr[1][1] * arr1[1][0];
+    arr3[1][1] = arr[1][0] * arr1[0][1] + arr[1][1] * arr1[1][1];
+    printf("The product of matrices\n");
+    for (i = 0; i < 2; i++)
     {
-        for (int j = 0; j < 2; j++){
-            scanf("%d", &arr2[i][j]);
-        }
-    }
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++){
-            printf("%d ", arr2[i][j]);
-        }
+        for (j = 0; j < 2; j++)
+            printf("%d  ", arr3[i][j]);
         printf("\n");
     }
-    printf("sum matrix:\n");
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++){
-            sum = arr[i][j] + arr2[i][j];
-            printf("%d ", sum);
-        }
-        printf("\n");
-    }
-    printf("Product matrix:\n");
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++){
-            prod = arr[i][j] * arr2[i][j];
-            printf("%d ", prod);
-        }
-        printf("\n");
-    }
-
     return 0;
 }
